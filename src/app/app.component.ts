01d86f532e5a75521522367a7140683b84d3e80a
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-02-08 17:28:32 +0100

    Petites modifs du visuel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent {
   readonly sigH1 = signal<number>(5);
   readonly sigM1 = computed<Matrix<number, number, number>>(() => initMatrixIntRandom<number, number>(this.sigH1(), this.sigL1()));
 
-  readonly sigL2 = signal<number>(10);
-  readonly sigH2 = signal<number>(10);
+  readonly sigL2 = signal<number>(5);
+  readonly sigH2 = signal<number>(5);
   readonly sigM2 = computed<Matrix<number, number, number>>(() => initMatrixIntRandom<number, number>(this.sigH2(), this.sigL2()));
 
   readonly sigM1plusM2 = computed<Matrix<number, number, number>>(() => addIntMatrixes(this.sigM1(), this.sigM2()));
