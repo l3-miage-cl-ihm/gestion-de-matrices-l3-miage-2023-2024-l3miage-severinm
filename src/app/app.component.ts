69c8365ff8e922462239f3c830d5470e2ed292c1
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-02-08 17:23:46 +0100

    Étape 4 : Un peu plus d'interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 import { Matrix, initMatrixIntRandom, addIntMatrixes, multiplyIntMatrixes } from './matrix';
+import { Highlight } from './matrix/matrix.component';
 
 @Component({
   selector: 'app-root',
@@ -20,4 +21,21 @@ export class AppComponent {
 
   readonly sigM1plusM2 = computed<Matrix<number, number, number>>(() => addIntMatrixes(this.sigM1(), this.sigM2()));
   readonly sigM1xM2 = computed<Matrix<number, number, number>>(() => multiplyIntMatrixes(this.sigM1(), this.sigM2()));
+
+  readonly sigHhilightInM1 = signal<Highlight>(undefined);
+  readonly sigHhilightInM2 = signal<Highlight>(undefined);
+  readonly sigHhilightInM1plusM2 = signal<Highlight>(undefined);
+  readonly sigHhilightInM1xM2 = signal<Highlight>(undefined);
+
+  overM1plusM2( c?: [line: number, column: number] ): void {
+    this.sigHhilightInM1plusM2.set(c === undefined ? undefined : {cell: [c[0], c[1]]});
+    this.sigHhilightInM1.set(c === undefined ? undefined : {cell: [c[0], c[1]]});
+    this.sigHhilightInM2.set(c === undefined ? undefined : {cell: [c[0], c[1]]});
+  }
+
+  overM1xM2( c?: [line: number, column: number] ): void {
+    this.sigHhilightInM1xM2.set(c === undefined ? undefined : {cell: [c[0], c[1]]});
+    this.sigHhilightInM1.set(c === undefined ? undefined : {line: c[0]});
+    this.sigHhilightInM2.set(c === undefined ? undefined : {column: c[1]});
+  }
 }
--- src/app/matrix/matrix.component.ts
+++ src/app/matrix/matrix.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Matrix, initMatrixIntRandom, getMatrixLine, getMatrixColumn } from '../matrix';
 
 @Component({
@@ -11,6 +11,10 @@ export class MatrixComponent {
 
   @Input({required: true}) matrix: Matrix<number, number, number> = initMatrixIntRandom<number, number>(0, 0);
 
+  @Input() highlight: Highlight;
+
+  @Output() pointerOver = new EventEmitter<[line: number, column: number]>(); 
+
   range(n: number): number[] {
     let array = [];
     for (let i = 0; i < n; i++)
@@ -18,6 +22,24 @@ export class MatrixComponent {
     return array;
   }
 
+  isHighlighted(line: number, column: number): boolean {
+    if ('cell' in this.highlight! && this.highlight.cell[0] === line && this.highlight.cell[1] === column)
+      return true;
+    else if ('line' in this.highlight! && this.highlight.line === line)
+      return true;
+    else if ('column' in this.highlight! && this.highlight.column === column)
+      return true;
+    return false;
+  }
+
   getMatrixLine = getMatrixLine;
   getMatrixColumn = getMatrixColumn;
 }
+
+export type HighlightLine = {line: number};
+export type HighlightColumn = {column: number};
+export type HighlightCell = {cell: [line: number, column: number]};
+export type Highlight = undefined
+                      | HighlightLine
+                      | HighlightColumn
+                      | HighlightCell;
